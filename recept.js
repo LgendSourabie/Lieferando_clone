28f0f47d3f1f78b75d546c317aa93ed957334e9c
Ibrahima SOURABIE <ge65yub@mytum.de>
2023-11-18 10:31:58 +0100

    update landingpage

---
+++ recept.js
@@ -0,0 +1,239 @@
+'use strict';
+
+const appetisers = [
+  {
+    name: 'Makhni Dal',
+    description: 'Linsen nach indischer Art',
+    price: 14.5,
+  },
+  {
+    name: 'Alu Palak',
+    description: 'Spinat mit Kartoffeln nach indischer Art',
+    price: 14.5,
+  },
+  {
+    name: 'Mixed Vegetables',
+    description: 'gemischtes Gemüse mit Currysauce',
+    price: 15.9,
+  },
+  {
+    name: 'Palak Panir',
+    description: 'frischer Spinat mit hausgemachtem Käse',
+    price: 15.9,
+  },
+  {
+    name: 'Mushroom Mattar Masala',
+    description: 'frische Champignons und Erbsen gebraten in Curry-Masalasauce',
+    price: 15.9,
+  },
+  {
+    name: 'Mixed Vegetables',
+    description: 'gemischtes Gemüse mit Currysauce',
+    price: 15.9,
+  },
+  {
+    name: 'Chili Panir (scharf)',
+    description: 'mit hausgemachter Käse in scharfer indischer Chilisauce',
+    price: 12.9,
+  },
+
+  {
+    name: 'Alu Gobi  ',
+    description: 'Kartoffeln und Blumenkohl nach Punjabi Art',
+    price: 15.9,
+  },
+  {
+    name: 'Mango Beef',
+    description: 'Rindfleisch in fein würziger Mangosauce',
+    price: 13.9,
+  },
+  {
+    name: 'Beef Masala',
+    description: 'Rindfleisch mit Masalasauce',
+    price: 13.9,
+  },
+  {
+    name: 'Chicken Curry',
+    description: 'Hühnerbrustfilet in kräftiger Currysauce',
+    price: 13.9,
+  },
+
+  {
+    name: 'Chicken Jalfrezi',
+    description: 'Hühnerbrustfilet in exotisch pikanter Sauce mit Zitronensaft',
+    price: 12.9,
+  },
+
+  {
+    name: 'Chicken Palak',
+    description: 'Hühnerbrustfilet in Spinatkreation',
+    price: 12.9,
+  },
+  {
+    name: 'Chili Chicken (scharf)',
+    description: 'Hühnerbrustfilet in indischer Chilisauce',
+    price: 12.9,
+  },
+  {
+    name: 'Chicken Tikka',
+    description:
+      'im Tandoori gegrilltes mariniertes Hühnerbrustfilet mit Tandoori-Sauce',
+    price: 13.5,
+  },
+  {
+    name: 'Tandoori Chicken',
+    description:
+      'in besonderer Gewürzmischung und Joghurt mariniertes Hühnchen, im Tandoori gegrillt mit Tandoori-Sauceprice',
+    price: 13.5,
+  },
+  {
+    name: 'Butter Chicken',
+    description: 'gegrilltes Hühnerbrustfilet in Tomaten-Buttercremesauce',
+    price: 13.5,
+  },
+  {
+    name: 'Chicken Tikka Masala',
+    description: 'im Tandoor gegrilltes Hühnerbrustfilet in Masalasauce',
+    price: 13.5,
+  },
+  {
+    name: 'Lamm Curry',
+    description: 'Lammfleisch in kräftiger Sauce',
+    price: 13.9,
+  },
+  {
+    name: 'Lamm Vindalu (sehr scharf)',
+    description: 'Lammfleisch mit Kartoffeln in sehr scharfer Sauce',
+    price: 13.9,
+  },
+  {
+    name: 'Dahi Gosht',
+    description: 'Lammfleisch in würziger Joghurtsauce',
+    price: 13.9,
+  },
+  {
+    name: 'Fisch Curry',
+    description: 'Fischfilet in kräftiger Sauce',
+    price: 12.5,
+  },
+  {
+    name: 'Mango Chicken',
+    description: 'Hühnerbrustfilet in feiner Mangosauce',
+    price: 12.5,
+  },
+  {
+    name: 'Coconut Chicken',
+    description:
+      'gegrillte Hühnerbrustfilet und verschiedenes Gemüse in würziger Currysauce mit Kokosmilch verfeinert',
+    price: 13.5,
+  },
+  {
+    name: 'Coconut Ente',
+    description:
+      'knusprige Ente auf würziger Kokos-Currysauce mit verschiedenem Gemüse',
+    price: 15.5,
+  },
+  {
+    name: 'Chicken mit Erdnusssoße',
+    description: 'Chicken in Erdnusssoße, serviert mit Gemüse und Reis',
+    price: 13.5,
+  },
+];
+
+const chicken_specialities = [
+  {
+    name: 'Chicken Curry',
+    description: 'Hühnerbrustfilet in kräftiger Currysauce',
+    price: 15.9,
+  },
+  {
+    name: 'Chicken Jalfrezi',
+    description:
+      'Hühnerbrustfilet in exotisch, pikanter Sauce mit Zitronenspritzern',
+    price: 16.9,
+  },
+  {
+    name: 'Chicken Palak',
+    description: 'Hühnerbrustfilet in Spinatkreation',
+    price: 16.9,
+  },
+  {
+    name: 'Butter Chicken',
+    description: 'gegrilltes Hühnerbrustfilet in Tomaten-Buttercremesauce',
+    price: 15.9,
+  },
+  {
+    name: 'Chicken Vindalu (sehr scharf)',
+    description: 'Hühnerbrustfilet mit Kartoffeln in sehr scharfer Sauce',
+    price: 16.9,
+  },
+  {
+    name: 'Chili Chicken (scharf)',
+    description: 'Hühnerbrustfilet in indischer Chilisauce',
+    price: 16.9,
+  },
+  {
+    name: 'Chicken Korma',
+    description: 'Hühnerbrustfilet in Joghurt-Cashew-Cremesauce',
+    price: 16.9,
+  },
+  {
+    name: 'Chicken Sabzi',
+    description:
+      'zarte Hühnerfleischstücke aus dem Lehmofen mit verschiedenem frischen Gemüse in Masalasauce',
+    price: 16.9,
+  },
+  {
+    name: 'Mango Chicken',
+    description: 'Hühnerbrustfilet in einer feinen Mangosauce',
+    price: 16.9,
+  },
+  {
+    name: 'Chicken Tikka Masala',
+    description:
+      'in Tandoor gegrilltes Hühnerbrustfilet in spezieller Masalasauce, Spezialität des Hauses',
+    price: 15.9,
+  },
+  {
+    name: 'Chicken Chana Masala',
+    description: 'Kichererbsen mit Hühnchen nach Punjabi Art',
+    price: 16.9,
+  },
+  {
+    name: 'Chicken Badam Pasanda',
+    description:
+      'Hähnchenbrustfilet im Tandoor gegrillt mit Joghurt-Cashew-Cremesauce',
+    price: 16.9,
+  },
+  {
+    name: 'Coconut Chicken',
+    description:
+      'gegrillte Hühnerbrustfilet und verschiedenes Gemüse in würziger Currysauce mit Kokosmilch verfeinert',
+    price: 16.9,
+  },
+  {
+    name: 'Chicken Garlic Tikka',
+    description: 'gegrilltes mariniertes Hühnerbrustfilet mit Knoblauch',
+    price: 16.9,
+  },
+  {
+    name: 'Chicken mit Erdnusssoße',
+    description: 'Chicken mit Erdnusssoße, serviert mit Gemüse und Reis',
+    price: 16.9,
+  },
+];
+
+const recepts = [
+  {
+    nameR: 'Mittagsmenüs',
+    dataR: appetisers,
+    imageR: './images/mittag.jpg',
+    descriptionR: 'Alle Gerichte werden mit Basmatireis serviert.',
+  },
+  {
+    nameR: 'Chicken Spezialitäten',
+    dataR: chicken_specialities,
+    imageR: './images/india_chicken.jpeg',
+    descriptionR: 'Alle Gerichte werden mit Basmatireis serviert.',
+  },
+];
--- script.js
+++ script.js
@@ -1,77 +1,171 @@
-'use strict'
+'use strict';
 
-const receipes = [
-        {
-          "name": "Spaghetti Carbonara",
-          "description": "Classic Italian pasta dish with eggs, cheese, pancetta, and black pepper.",
-          "price": "12.99 €",
-          "ingredients": ["spaghetti", "eggs", "pecorino cheese", "pancetta", "black pepper"]
-        },
-        {
-          "name": "Grilled Salmon",
-          "description": "Freshly grilled salmon fillet with lemon herb sauce.",
-          "price": "16.49 €",
-          "ingredients": ["salmon fillet", "lemon", "herbs", "olive oil", "salt"]
-        },
-        {
-          "name": "Mushroom Risotto",
-          "description": "Creamy Arborio rice cooked with mushrooms and white wine.",
-          "price": "11.95 €",
-          "ingredients": ["Arborio rice", "mushrooms", "white wine", "onion", "parmesan cheese"]
-        },
-        {
-          "name": "Chicken Parmesan",
-          "description": "Breaded chicken cutlet topped with marinara sauce and melted mozzarella.",
-          "price": "14.75 €",
-          "ingredients": ["chicken cutlet", "marinara sauce", "mozzarella cheese", "breadcrumbs"]
-        },
-        {
-          "name": "Caesar Salad",
-          "description": "Classic Caesar salad with romaine lettuce, croutons, parmesan cheese, and Caesar dressing.",
-          "price": "8.99 €",
-          "ingredients": ["romaine lettuce", "croutons", "parmesan cheese", "Caesar dressing"]
-        },
-        {
-          "name": "Margherita Pizza",
-          "description": "Traditional pizza with tomato sauce, fresh mozzarella, basil, and olive oil.",
-          "price": "10.50 €",
-          "ingredients": ["pizza dough", "tomato sauce", "fresh mozzarella", "basil", "olive oil"]
-        },
-        {
-          "name": "Beef Burger",
-          "description": "Juicy beef patty with lettuce, tomato, onion, and special sauce in a sesame seed bun.",
-          "price": "9.25 €",
-          "ingredients": ["beef patty", "sesame seed bun", "lettuce", "tomato", "onion"]
-        },
-        {
-          "name": "Vegetable Stir-Fry",
-          "description": "Assorted vegetables stir-fried in a savory soy-ginger sauce.",
-          "price": "10.95 €",
-          "ingredients": ["assorted vegetables", "soy sauce", "ginger", "garlic", "sesame oil"]
-        },
-        {
-          "name": "Tiramisu",
-          "description": "Classic Italian dessert made with layers of coffee-soaked ladyfingers and mascarpone cheese.",
-          "price": "6.75 €",
-          "ingredients": ["ladyfingers", "mascarpone cheese", "coffee", "cocoa powder"]
-        }
-      ] ;
+const render = function (receptType) {
+  let receptContainer = document.getElementById('recept-container');
+  receptContainer.innerHTML = '';
 
-function render(){
-let menuContainer = document.getElementById('menu-container')
-menuContainer.innerHTML = '' ;
-for(let i=0; i<receipes.length ;i++){
-    const receipe = receipes[i]
-    menuContainer.innerHTML += `<div class="receipe" id=receipe-${i}>
-                                    <div class="directionHorizontal">
-                                        <h3>${receipe.name}</h3>
-                                        <button class="round-plus-button"><img class="plusButton" src="icons/plus-solid.svg" alt="add button"></button>
-                                    </div>
-                                    <p>${receipe.description}</p>
-                                    <h2 class="price">${receipe.price}</h2>
-                                </div> `
+  for (let j = 0; j < receptType.length; j++) {
+    const recept = receptType[j];
 
-}
+    receptContainer.innerHTML += /*html*/ `<div class="recept-image-description">
+    <div id="image-recept-type_${j}" class="image-recept-type"></div>  
+        <p class="arial p-margin recept-type-name">${recept['nameR']}</p>
+        <p class="font-12 grey p-margin">${recept['descriptionR']}</p>  
+    </div> 
+ `;
+    document.getElementById(
+      `image-recept-type_${j}`
+    ).style.backgroundImage = `url('${recept['imageR']}')`;
+
+    subRender(receptContainer, recept);
+  }
+};
+
+function subRender(receptContainer, receptType) {
+  const receptR = receptType['dataR'];
+  for (let i = 0; i < receptR.length; i++) {
+    const recepttype = receptR[i];
+    receptContainer.innerHTML += /*html*/ `
+        <div id="recept-container_${i}" class="recept-type">
+        <div class="name-line">
+            <div class="name-info-grp">
+                <p class="p-margin"><b>${recepttype['name']}</b></p>
+                <img class="icons-shape" src="./icons/circle-info-solid.svg" alt="Info">
+            </div>
+            <img class="icons-shape"  src="./icons/plus-solid.svg" alt="Hinzufügen">
+        </div>
+        <p class="p-margin grey">${recepttype['description']}</p>
+        <p class='price p-margin'><b>${germanPreis(
+          recepttype['price']
+        )} €</b></p>
+    </div>
+        `;
+  }
 }
 
-function loadMenu(){}
+const germanPreis = function (price) {
+  let toString = String(price.toFixed(2));
+  return toString.replace('.', ',');
+};
+
+const shipping = function (Id) {
+  let id = document.getElementById(Id);
+  let deliverId = document.getElementById('delivery-button');
+  let takeId = document.getElementById('take-button');
+  takeId.style.backgroundColor = 'rgb(239, 237, 234)';
+  deliverId.style.backgroundColor = 'rgb(239, 237, 234)';
+  document.getElementById('deliver-bicycle').style.filter =
+    'invert(0%) sepia(100%) saturate(7500%) hue-rotate(189deg) brightness(111%) contrast(108%)';
+  document.getElementById('take-cart').style.filter =
+    'invert(0%) sepia(100%) saturate(7500%) hue-rotate(189deg) brightness(111%) contrast(108%)';
+  id.style.backgroundColor = 'white';
+  id.childNodes.item(1).style.filter =
+    'invert(40%) sepia(81%) saturate(1285%) hue-rotate(1deg) brightness(98%) contrast(97%)';
+};
+
+const restaurantLocation = function (Id, Address) {
+  let location = document.getElementById('deliver-cost-line');
+  location.innerHTML = '';
+  if (Id === 'take-button') {
+    location.innerHTML = /*html*/ ` <img class="icons-shape-smaller " src="./icons/location.svg" alt="Location"/> <p class="p-margin grey">${Address}</p>`;
+  } else if (Id === 'delivery-button') {
+    location.innerHTML = `
+    <img
+    class="icons-shape-small"
+    src="./icons/cart-shopping-solid.svg"
+    alt=""
+  />
+  <p id="location" class="p-margin grey">
+    Min. 18,90 € <b>&#183;</b>
+  </p>
+  <img
+    class="icons-shape-small bg-green"
+    src="./icons/bicycle-solid.svg"
+    alt=""
+  />
+  <p class="p-margin green">Kostenlos</p>
+    `;
+  }
+};
+
+const searchRecept = function () {
+  let search = document.getElementById('menu-type-line');
+  search.style.display = 'block';
+  search.innerHTML = '';
+  search.innerHTML = /*html*/ `
+  <p class="p-margin"> <b>Suche im Menü</b></p>
+  <div class="inline-input"> 
+  <input class="input" type="text" placeholder="Suche nach Gerichten, usw...">
+  <img id="close-button" onclick="closeButton()" class="icons-shape-small close-button" src="./icons/close.svg" alt="">
+  </div>`;
+};
+
+const closeButton = function () {
+  let search = document.getElementById('menu-type-line');
+  search.style.display = 'flex';
+  search.innerHTML = '';
+  search.innerHTML = /*html*/ `
+     <img
+              class="icons-shape search"
+              src="/icons/search.svg"
+              alt="Suche"
+              onclick="searchRecept()"
+            />
+            <button class="menu-button arial font-14 button-activate">
+              Mittagsmenüs
+            </button>
+            <button class="menu-button arial font-14 button-desactivate">
+              Chicken Spezialitäten
+            </button>
+            <button class="menu-button arial font-14 button-desactivate">
+              Vorspeisen
+            </button>
+            <button class="menu-button arial font-14 button-desactivate">
+              Ente Spezialitäten
+            </button>
+            <button class="menu-button arial font-14 button-desactivate">
+              Vegane Spezialitäten
+            </button>
+            <button class="menu-button arial font-14 button-desactivate">
+              Dessert
+            </button>
+            <button class="menu-button arial font-14 button-desactivate">
+              Getränke
+            </button>
+    `;
+};
+
+const fillCart = () => {
+  let priceSection = document.getElementById('price-calc-section');
+  priceSection.innerHTML = '';
+  priceSection.innerHTML = `Hallo`;
+};
+
+render(recepts);
+
+{
+  /* <table>
+    <tr>
+        <td>Number</td>
+        <td>Nme</td>
+        <td></td>
+        <td></td>
+        <td>Price</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>Anmerkung <br>hinzufügen</td>
+        <td> <button><img src="minus" alt=""></button></td>
+        <td>number</td>
+        <td><button><img src="minus" alt=""></button></td>
+    </tr>
+</table>
+<div>
+    <div><p>Zwischensumme</p> <p>Zsum</p></div>
+    <div><p>Lieferkosten</p> <p>Kostenlos</p></div>
+    <div><p>Gesamt</p> <p><b>sum</b></p></div>
+</div>
+<button>
+    Bezahlen(sum)
+</button> */
+}
